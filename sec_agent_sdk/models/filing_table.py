29f90cd51452cf18d495e351e049a83092d1a762
Jojo Ortiz <ortiz.jroman@gmail.com>
2024-06-10 18:47:36 -0700

    add BUSINESS_SEGMENTS_AND_KPIS to FilingTable

--- sec_agent_sdk/models/filing_table.py
+++ sec_agent_sdk/models/filing_table.py
@@ -29,6 +29,8 @@ class FilingTable(str, Enum):
     BALANCE_SHEET = 'balance-sheet'
     CASH_FLOW_STATEMENT = 'cash-flow-statement'
     INCOME_STATEMENT = 'income-statement'
+    BUSINESS_SEGMENTS_AND_KPIS = "business-segments-and-kpis"
+
 
     @classmethod
     def from_json(cls, json_str: str) -> Self:
